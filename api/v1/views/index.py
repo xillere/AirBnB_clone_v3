52933eae60ae0a83fea242972944db4db928f18e
Chideraa <chideraaozobu@gmail.com>
2024-02-27 04:48:09 +0200

    stats of todo objects

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,7 +3,12 @@
 
 import models
 from models import storage
-from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Reviewl
 from flask import Blueprint, render_template, jsonify
 from api.v1.views import app_views
 
