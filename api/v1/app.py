fab44459f9ab395e6e598798c4c87f59c522caa0
wallace <exnesswallace@gmail.com>
2024-02-27 04:11:19 +0200

    fixed pycodestyle and pep8

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
+"""register the blueprint app_views to your Flask instance app"""
 
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
 from os import getenv
+
 app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
-def sto():
+def sto(exception):
+    """method to handle @app.teardown_appcontext
+    that calls storage.close()"""
+
     storage.close()
+
 
 if __name__ == "__main__":
     host = getenv("HBNB_API_HOST")
